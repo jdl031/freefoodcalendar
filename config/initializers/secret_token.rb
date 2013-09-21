842775d5a193e2e5a84f380a1350e9329ef31600
James Lloyd <james.jd.lloyd@gmail.com>
2013-09-21 16:32:29 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freefoodcalendar::Application.config.secret_key_base = '7f4d5afa00c18ba9eec210e9ceaffb5631e51d2e5e0839765b967bbf0d897d6b35694ab5335068334b0f4da26d995d2812642b789c589e8a92239d38d20b6e0c'
